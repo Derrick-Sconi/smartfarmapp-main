c5fc70993a7e0cdebc0342eaf80670f6c1ba647e
Okai Yeboah <kingkloy16@gmail.com>
2021-08-23 18:59:02 +0000

    add authenticatio wrapper

--- lib/src/screens/views/loadingscreen.dart
+++ lib/src/screens/views/loadingscreen.dart
@@ -1,7 +1,10 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../providers/auth_provider.dart';
 import '../widgets/loading.dart';
+import 'bottom_navigation.dart';
 import 'login_page.dart';
 
 class LoadingScreen extends StatelessWidget {
@@ -12,10 +15,27 @@ class LoadingScreen extends StatelessWidget {
       body: AnimatedSplashScreen(
         duration: 3000,
         splash: Loading(),
-        nextScreen: LoginPage(),
+        nextScreen: AuthenticationWrapper(),
         splashTransition: SplashTransition.fadeTransition,
         backgroundColor: Colors.white,
       ),
+    );
+  }
+}
+
+class AuthenticationWrapper extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final _authState = watch(authStateProvider);
+
+    return _authState.map(
+      data: (data) {
+        print(data);
+        if (data.value == null) return LoginPage();
+        return BottomNavigationScreen();
+      },
+      loading: (_) => Loading(),
+      error: (e) => Text('$e'),
     );
   }
 }
