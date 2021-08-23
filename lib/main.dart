c3318465c9d76e762480567ddf6a847c97678818
Okai Yeboah <kingkloy16@gmail.com>
2021-08-23 18:54:02 +0000

    init firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'src/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // init Firebase app
+  await Firebase.initializeApp();
+  // disable landscape mode or screen auto-rotate
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(SmartFarmApp());
 }
 
