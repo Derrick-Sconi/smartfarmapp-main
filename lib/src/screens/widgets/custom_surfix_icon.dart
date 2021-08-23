beecc6098ee06ad4780350b8affd1b07bfde5d9d
Okai Yeboah <kingkloy16@gmail.com>
2021-08-23 19:02:15 +0000

    refactor

--- lib/src/screens/widgets/custom_surfix_icon.dart
+++ lib/src/screens/widgets/custom_surfix_icon.dart
@@ -1,27 +1,23 @@
 import 'package:flutter/material.dart';
+
 import 'sizeconfiguration.dart';
 
 class CustomSurffixIcon extends StatelessWidget {
-  const CustomSurffixIcon({
-    required Key key,
-    required this.svgIcon,
-  }) : super(key: key);
+  const CustomSurffixIcon({Key? key, required this.svgIcon}) : super(key: key);
 
   final String svgIcon;
 
   @override
   Widget build(BuildContext context) {
+    final sizeConfig = SizeConfig(context);
     return Padding(
       padding: EdgeInsets.fromLTRB(
         0,
-        getProportionateScreenWidth(20),
-        getProportionateScreenWidth(20),
-        getProportionateScreenWidth(20),
+        sizeConfig.getProportionateScreenWidth(20),
+        sizeConfig.getProportionateScreenWidth(20),
+        sizeConfig.getProportionateScreenWidth(20),
       ),
-      child: Image.asset(
-        svgIcon,
-        height: getProportionateScreenWidth(18),
-      ),
+      child: Icon(Icons.mail),
     );
   }
 }
